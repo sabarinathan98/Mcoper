2b5aa3b08ca9fb097f155cb424bc18d1382f2ed0
sabarinathan98 <sabarinathan4028@gmail.com>
2021-07-22 22:58:27 +0530

    Sss

---
+++ src/test/java/org/ol/po/CommitSection.java
@@ -0,0 +1,19 @@
+package org.ol.po;
+
+public class CommitSection {
+	public void add()
+	{
+		System.out.println("Added");
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CommitSection m=new CommitSection();
+		m.add();
+		
+		
+
+	}
+
+}
